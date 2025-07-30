d899b3d115891b1c4468128c3a5b4515b19029c5
Mohammed Amaan Ahmad <115135095+Mohammedamaan5714@users.noreply.github.com>
2025-07-30 18:34:20 +0530

    [#99] show category suggestion on empty favouriate page (#112)

--- app/favorite/page.tsx
+++ app/favorite/page.tsx
@@ -4,8 +4,8 @@ import BackButton from "@/components/BackButton";
 import { PlusIcon } from "@/components/Icons";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { CATEGORIES_URL } from "@/lib/urls";
 
 interface Meal {
   idMeal: string;
@@ -13,14 +13,33 @@ interface Meal {
   strMealThumb: string;
 }
 
+interface Category {
+  idCategory: string;
+  strCategory: string;
+  strCategoryThumb: string;
+  strCategoryDescription: string;
+}
+
 export default function FavoritesPage() {
   const [favorites, setFavorites] = useState<Meal[]>([]);
+  const [categories, setCategories] = useState<Category[]>([]);
 
   useEffect(() => {
     const storedFavorites = localStorage.getItem("favorites");
     if (storedFavorites) {
       setFavorites(JSON.parse(storedFavorites));
     }
+  }, []);
+
+  useEffect(() => {
+    fetch(CATEGORIES_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setCategories(data.categories);
+      })
+      .catch((error) => {
+        console.error("Error fetching categories:", error);
+      });
   }, []);
 
   const removeFavorite = (idMeal: string) => {
@@ -31,12 +50,53 @@ export default function FavoritesPage() {
 
   return (
     <div className="p-6 min-h-screen bg-base-100">
+      <BackButton />
       <h1 className="text-3xl md:text-5xl font-bold text-center text-secondary mb-10">
         Your Favorite Meals 💖
       </h1>
 
       {favorites.length === 0 ? (
-        <p className="text-center text-lg">No favorites yet!</p>
+        <>
+          <p className="text-center text-lg mb-6">No favorites yet!</p>
+          <section className="categories-section flex flex-col items-center justify-center p-5 md:p-10 w-full bg-gradient-to-br from-amber-50 to-amber-100 rounded-lg shadow-lg">
+            <h2 className="text-xl md:text-3xl text-base-content mb-10 font-semibold text-center">
+              A Taste for Every Mood and Moment
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-full max-w-7xl">
+              {categories.map((category) => (
+                <div
+                  key={category.idCategory}
+                  className="card card-compact w-full bg-base-100 shadow-xl rounded-lg overflow-hidden transform transition duration-300 hover:shadow-2xl hover:scale-105 hover:-translate-y-1 cursor-pointer"
+                >
+                  <figure>
+                    <img
+                      src={category.strCategoryThumb}
+                      alt={category.strCategory}
+                      className="w-full h-48 object-cover"
+                    />
+                  </figure>
+                  <div className="card-body p-4">
+                    <h3 className="card-title text-lg md:text-xl text-gray-800 flex items-center">
+                      <PlusIcon />
+                      {category.strCategory}
+                    </h3>
+                    <p className="text-sm md:text-base text-gray-600">
+                      {category.strCategoryDescription.slice(0, 150) + " ..."}
+                    </p>
+                    <Link
+                      className="card-actions justify-end"
+                      href={`/category/${category.strCategory}`}
+                    >
+                      <button className="btn btn-primary text-white text-sm md:text-base shadow-md">
+                        Explore
+                      </button>
+                    </Link>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </section>
+        </>
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6 justify-items-center">
           {favorites.map((meal) => (
